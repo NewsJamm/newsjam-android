1f6455253267ee7d8623612b90966f6e9459444e
jiwon <pjw99032986@gmail.com>
2024-10-07 03:52:08 +0900

    Feat : 로그인 프레그먼트

---
+++ app/src/main/java/com/example/newsjam_android/ui/view/activity/account/LoginFragment.kt
@@ -0,0 +1,17 @@
+package com.example.newsjam_android.ui.view.activity.account
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newsjam_android.R
+import com.example.newsjam_android.databinding.FragmentLoginBinding
+import com.example.newsjam_android.ui.base.BaseFragment
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
+
+    override fun setLayout() {
+        ""
+    }
+}
