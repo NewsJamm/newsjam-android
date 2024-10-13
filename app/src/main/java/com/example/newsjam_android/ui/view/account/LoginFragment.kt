8f140c30d4b87f3cbd6d20713f4cbef7842a53c7
jiwon <pjw99032986@gmail.com>
2024-10-13 18:52:30 +0900

    feat : 로그인 화면 기능코딩 (화면 이동)

--- app/src/main/java/com/example/newsjam_android/ui/view/account/LoginFragment.kt
+++ app/src/main/java/com/example/newsjam_android/ui/view/account/LoginFragment.kt
@@ -1,10 +1,11 @@
-package com.example.newsjam_android.ui.view.activity.account
+package com.example.newsjam_android.ui.view.account
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.newsjam_android.R
 import com.example.newsjam_android.databinding.FragmentLoginBinding
 import com.example.newsjam_android.ui.base.BaseFragment
@@ -12,6 +13,12 @@ import com.example.newsjam_android.ui.base.BaseFragment
 class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
 
     override fun setLayout() {
-        ""
+        setOnClickView()
+    }
+
+    private fun setOnClickView() {
+        binding.activityLoginSocial1Bt.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_likeChoiceFragment)
+        }
     }
 }
