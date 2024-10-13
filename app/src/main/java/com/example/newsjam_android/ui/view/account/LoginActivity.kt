201cb6e026162f0313db00345f9a38b807dbe47e
jiwon <pjw99032986@gmail.com>
2024-10-13 18:52:43 +0900

    feat : 로그인 화면 기능코딩 (화면 이동)

---
+++ app/src/main/java/com/example/newsjam_android/ui/view/account/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.example.newsjam_android.ui.view.account
+
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.newsjam_android.R
+import com.example.newsjam_android.databinding.ActivityLoginBinding
+import com.example.newsjam_android.ui.base.BaseActivity
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+
+    private lateinit var navController: NavController
+    override fun setLayout() {
+        val host = supportFragmentManager.findFragmentById(binding.activityLoginContainerView.id) as NavHostFragment
+        navController = host.navController
+    }
+
+}
