f088663e447c2607b0cd351314697983a4ad861b
jiwon <pjw99032986@gmail.com>
2024-10-21 13:58:14 +0900

    feat : 홈 페이지 필요한 어댑터 추가

---
+++ app/src/main/java/com/example/newsjam_android/data/model/NewsItem.kt
@@ -0,0 +1,8 @@
+package com.example.newsjam_android.data.model
+
+data class NewsItem (
+    val profile : String,
+    val creator : String,
+    val title : String,
+    val arriveTime : String,
+)
