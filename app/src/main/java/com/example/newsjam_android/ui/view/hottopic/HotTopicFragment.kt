16d4e34ecbb6d3e05aae792b5330c45887a643cc
jiwon <pjw99032986@gmail.com>
2024-10-21 14:00:57 +0900

    feat : 핫토픽 메인 프레그먼트 추가 (홈)

--- app/src/main/java/com/example/newsjam_android/ui/view/hottopic/HotTopicFragment.kt
+++ app/src/main/java/com/example/newsjam_android/ui/view/hottopic/HotTopicFragment.kt
@@ -1,60 +1,64 @@
 package com.example.newsjam_android.ui.view.hottopic
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.anychart.charts.TagCloud
 import com.example.newsjam_android.R
+import com.example.newsjam_android.databinding.FragmentHotTopicBinding
+import com.example.newsjam_android.ui.base.BaseFragment
+import com.example.newsjam_android.ui.view.adapter.HotTopicViewPagerAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HotTopicFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class HotTopicFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+class HotTopicFragment : BaseFragment<FragmentHotTopicBinding>(R.layout.fragment_hot_topic) {
+    private lateinit var viewPagerAdapter: HotTopicViewPagerAdapter
+    private lateinit var fragmentList: ArrayList<Fragment>
+    private lateinit var tabList : List<String>
+    override fun setLayout() {
+        setList()
+        initViewPager2()
+        initTabBar()
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hot_topic, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HotTopicFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HotTopicFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun setList(){
+        tabList = listOf("핫토픽", "카테고리 1", "카테고리 2", "카테고리 3",  "카테고리 4")
+        fragmentList = ArrayList()
+            for (i in tabList.indices){
+                fragmentList.add(HotTopicWordCloudFragment(tabList[i]))
             }
+        }
+    private fun initTabBar() {
+        TabLayoutMediator(binding.fragmentHotTopicTb, binding.fragmentHotTopicVp) { tab, position ->
+            tab.text = tabList[position]
+        }.attach()
+        setTabItemMargin(binding.fragmentHotTopicTb, 8)
+
+        binding.fragmentHotTopicTb.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(p0: TabLayout.Tab?) {
+            }
+
+        })
+    }
+    private fun initViewPager2() {
+        viewPagerAdapter = HotTopicViewPagerAdapter(requireActivity(),fragmentList)
+        binding.fragmentHotTopicVp.adapter = viewPagerAdapter
+    }
+    private fun setTabItemMargin(tabLayout: TabLayout, marginEnd: Int = 20) {
+        val tabs = tabLayout.getChildAt(0) as ViewGroup
+        for (i in 0 until tabs.childCount) {
+            val tab = tabs.getChildAt(i)
+            val lp = tab.layoutParams as LinearLayout.LayoutParams
+            lp.marginEnd = marginEnd
+            tab.layoutParams = lp
+            tabLayout.requestLayout()
+        }
     }
 }
