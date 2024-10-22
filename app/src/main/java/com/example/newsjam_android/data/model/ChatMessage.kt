4c872646fed896864b71079f5727ad19bd85443d
jiwon <pjw99032986@gmail.com>
2024-10-22 17:38:56 +0900

    feat : 채팅 타입 ai,human,app 에 따라 다르게 처리하는 데이터 클래스 선언

---
+++ app/src/main/java/com/example/newsjam_android/data/model/ChatMessage.kt
@@ -0,0 +1,7 @@
+package com.example.newsjam_android.data.model
+
+sealed class ChatMessage {
+    data class AIMessage(val message: String) : ChatMessage()
+    data class HumanMessage(val message: String) : ChatMessage()
+    data class AppMessage(val message: String, val topMessage : Boolean = false) : ChatMessage()
+}
