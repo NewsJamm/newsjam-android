a039df4de971def7395a7a5f049f008061a1fecc
jiwon <pjw99032986@gmail.com>
2024-10-24 16:34:21 +0900

    feat : 이전 페이지와 구분하기위해 Args 이용한 번들 데이터 추가

--- app/src/main/java/com/example/newsjam_android/ui/view/account/HotNewsConfirmFragment.kt
+++ app/src/main/java/com/example/newsjam_android/ui/view/account/HotNewsConfirmFragment.kt
@@ -1,6 +1,7 @@
 package com.example.newsjam_android.ui.view.account
 
 import android.content.Intent
+import android.view.View
 import com.example.newsjam_android.R
 import com.example.newsjam_android.databinding.HotNewsConfirmFragmentBinding
 import com.example.newsjam_android.ui.base.BaseFragment
@@ -14,6 +15,9 @@ class HotNewsConfirmFragment :
     }
 
     private fun setOnClickView() {
+        if (arguments?.getString("page") == "my_page"){
+            binding.appCompatButton.visibility = View.GONE
+        }
         binding.appCompatButton.setOnClickListener {
             startActivity(Intent(requireActivity(), MainActivity::class.java))
             requireActivity().finish()
