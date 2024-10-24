bf8a3b149e3c0e890bac52a99f20b6face19ced8
jiwon <pjw99032986@gmail.com>
2024-10-24 16:30:55 +0900

    feat : hilt, firebase 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     id("androidx.navigation.safeargs.kotlin")
+    id("com.google.gms.google-services")
+    id("com.google.dagger.hilt.android")
+    id("kotlin-kapt")
+
 }
 
 android {
@@ -43,7 +47,7 @@ android {
 
 dependencies {
 
-    implementation (libs.google.flexbox)
+    implementation(libs.google.flexbox)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -51,18 +55,28 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation (libs.androidx.viewpager2)
+    implementation(libs.androidx.viewpager2)
+    implementation(libs.firebase.messaging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.github.jolenechong:androidWordCloud:1.0.0") {
-        exclude(group="com.sun.xml.bind", module="jaxb-core")
-        exclude(group="com.sun.xml.bind", module="jaxb-impl")
+        exclude(group = "com.sun.xml.bind", module = "jaxb-core")
+        exclude(group = "com.sun.xml.bind", module = "jaxb-impl")
     }
-    implementation ("com.github.bumptech.glide:glide:4.9.0") {
-        exclude(group="com.android.support")
+    implementation("com.github.bumptech.glide:glide:4.9.0") {
+        exclude(group = "com.android.support")
     }
-    implementation (libs.support.fragment)
-    implementation ("com.github.AnyChart:AnyChart-Android:1.1.2")
+    implementation(libs.support.fragment)
+    implementation("com.github.AnyChart:AnyChart-Android:1.1.2")
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics.ktx)
+    implementation(libs.firebase.messaging)
+    implementation (libs.androidx.datastore.preferences)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
 
+}
+kapt {
+    correctErrorTypes = true
 }
